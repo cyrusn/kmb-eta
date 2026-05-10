b80f1b5fee9259b699a5fe47866b9ff2ec8c5d81
Cyrus Ngan <cyrusncy@gmail.com>
2026-05-11 00:52:57 +0800

    fix for cors problem

--- test_script.js
+++
@@ -1,6 +0,0 @@
-async function run() {
-    const stopsResp = await fetch(`https://data.etabus.gov.hk/v1/transport/kmb/route-stop/290A/outbound/1`);
-    const stopsJson = await stopsResp.json();
-    console.log("290A Outbound Stops:", stopsJson.data.slice(0, 3));
-}
-run();
